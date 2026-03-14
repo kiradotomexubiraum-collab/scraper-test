e19ff60db9152dbe5cdd4280dd595e1e61bb7d10
kiradotomexubiraum-collab <kiradotomexubiraum@gmail.com>
2026-03-14 15:53:43 -0300

    scraper.py

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-url = "https://example.com"
+url = "https://www.irani.delivery/pacaembu/ofertas-clube"
 
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
